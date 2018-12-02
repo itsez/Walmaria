4e3f4cfed0a3ff7038b3481acce7946c7029842d
John Hewitt <hewittjd@appstate.edu>
2018-12-02 13:21:58 -0500

    adding second page

---
+++ Walmaria/Walmaria/Walmaria/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Walmaria
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnStart(Button sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new Page1());
+        }
+    }
+}
---
+++ Walmaria/Walmaria/Walmaria/Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Walmaria
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+            var bev = new Image { Source = "Walmaria.images.bev.PNG" };
+            var keith = new Image { Source = "Walmaria.images.keith.PNG" };
+            var tyler = new Image { Source = "Walmaria.images.tyler.PNG" };
+            var andy = new Image { Source = "Walmaria.images.andy.PNG" };
+        }
+	}
+}
